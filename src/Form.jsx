ca051baf5e24737fe54a447de38d2d1bb3fca9fd
Nauti-Rohit <rohitnautiyal9@gmail.com>
2024-03-20 17:29:56 +0530

    Second Commit-'Handling Multiple Inputs'

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,36 +1,58 @@
 import { useState } from "react";
 
 export default function Form() {
-  let [fullName, setFullName] = useState("");
-  let [userName, setUserName] = useState("");
+  let [formData, setFormData] = useState({
+    fullName: "",
+    userName: "",
+    password: "",
+  });
 
-  let handleNameChange = (event) => {
-    setFullName(event.target.value);
+  let handelData = (event) => {
+    setFormData((currData) => {
+      return { ...currData, [event.target.name]: event.target.value };
+    });
   };
 
-  let handleUserName = (event) => {
-    setUserName(event.target.value);
+  let handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    setFormData({ fullName: "", userName: "", password: "" });
   };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="fullName">Full Name</label> &nbsp; : &nbsp; &nbsp;
       <input
         type="text"
         placeholder="Enter Name here"
-        value={fullName}
-        onChange={handleNameChange}
+        value={formData.fullName}
+        name="fullName"
+        onChange={handelData}
         id="fullName"
       ></input>
+      <br />
       <br />
       <label htmlFor="username">User Name</label> &nbsp; : &nbsp; &nbsp;
       <input
         type="text"
         placeholder="Enter UserName"
-        value={userName}
-        onChange={handleUserName}
+        value={formData.userName}
+        onChange={handelData}
         id="username"
+        name="userName"
       ></input>
+      <br />
+      <br />
+      <label htmlFor="password">Password</label> &nbsp; : &nbsp; &nbsp;
+      <input
+        type="password"
+        placeholder="Enter password here"
+        value={formData.password}
+        onChange={handelData}
+        id="password"
+        name="password"
+      ></input>
+      <br />
       <br />
       <br />
       <button>Submit</button>
